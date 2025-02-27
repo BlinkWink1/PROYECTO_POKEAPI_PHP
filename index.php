ceefbc5cc85d9342dd7765ee210deb2fb80843b0
ZakariaCB <118432598+BlinkWink1@users.noreply.github.com>
2025-02-27 05:35:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
 <?php
 
-require_once 'consts.php'; // Incluye el archivo de constantes
-require_once 'functions.php'; // Incluye el archivo de funciones
-require_once 'classes/Pokemon.php'; // Incluye la clase Pokemon
+require_once 'consts.php';
+require_once 'functions.php';
+require_once 'classes/Pokemon.php';
 
 // Obtener datos del primer Pokémon (Pikachu)
-$pokemon1 = Pokemon::fetch_and_create_pokemon(API_URL, 25); // Crea un objeto Pokemon para Pikachu
-$pokemon1_data = $pokemon1->get_data(); // Obtiene los datos de Pikachu como un array
+$pokemon1 = Pokemon::fetch_and_create_pokemon(API_URL, 25);
 
 // Obtener datos del segundo Pokémon (Charizard)
-$pokemon2 = Pokemon::fetch_and_create_pokemon(API_URL, 6); // Crea un objeto Pokemon para Charizard
-$pokemon2_data = $pokemon2->get_data(); // Obtiene los datos de Charizard como un array
+$pokemon2 = Pokemon::fetch_and_create_pokemon(API_URL, 6);
 ?>
 
-<?php render_template('head', ["title" => "Información de Pokémon"]); ?> 
+<?php render_template('head', ["title" => "Información de Pokémon"]); ?>
 
 <div class="container">
     <div class="row">
         <div class="col-md-6">
-            <?php render_template('main', $pokemon1_data); ?> 
+            <?php render_template('main', $pokemon1->get_data()); ?>
         </div>
         <div class="col-md-6">
-            <?php render_template('main', $pokemon2_data); ?> 
+            <?php render_template('main', $pokemon2->get_data()); ?>
         </div>
     </div>
-</div>
+</div>e
